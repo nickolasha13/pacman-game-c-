c677fe8b968f888c46fb836af3506383fbc33dc9
Mykola Bilous <68985080+nickolasha13@users.noreply.github.com>
2023-03-22 15:53:14 +0200

    start

---
+++ oop_lab/oop_lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace oop_lab
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DrawOutborders d= new DrawOutborders('#', new Point(1,1), 10, 10);
+            d.Draw();
+            DrawInborders fill = new DrawInborders('@', new Point(5, 5), 3, 3);
+            fill.Draw();
+        }
+    }
+}
